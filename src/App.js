9eb0d70c72fd3ac7c6ebbeaa45798b2cc1e5bcca
Ganesh <ganeshgitte4520@gmail.com>
2023-03-30 11:20:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import DropDownData from './components/DropDownData'
+import './App.css'
+
+
+const App = () => {
+
+  return (
+    <div id='App'>
+      <DropDownData />
+    </div>
+  );
+};
+
+export default App;
